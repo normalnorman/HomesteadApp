a57bd79b65726db0eb12293ebf5ea5e0f02a67ef
Blake Ball <blakesteve@gmail.com>
2014-02-11 17:11:09 -0600

    Product Detail View Controller

---
+++ ProductDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductDetailViewController.h
+//  PulseDemoApp
+//
+//  Created by Blake Ball on 2/11/14.
+//  Copyright (c) 2014 IBM MIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductDetailViewController : UIViewController
+
+@end
