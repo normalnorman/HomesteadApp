1bb25d811bdf43b5ee45a7fec3e439b2fb4235ad
Blake Ball <blakesteve@gmail.com>
2014-02-18 16:47:38 -0600

    Search Results: now with Color

--- PulseDemoApp/DepartmentViewController.m
+++ PulseDemoApp/DepartmentViewController.m
@@ -28,9 +28,16 @@
     return self;
 }
 
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIImage *logoImage = [self imageWithImage:[UIImage imageNamed:@"search_results"] scaledToSize:CGSizeMake(228, 12)];
+    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:logoImage];
+    
     
     self.productImages = [[NSMutableArray alloc] init];
 	// Do any additional setup after loading the view.
@@ -270,6 +277,17 @@
 {
     NSLog(@"Showing Detail View for customer");
     [self performSegueWithIdentifier: @"ProductDetailSegue" sender: self];
+}
+
+-(UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
+    //UIGraphicsBeginImageContext(newSize);
+    // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
+    // Pass 1.0 to force exact pixel size.
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 @end
